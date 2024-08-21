f35ec73976de6323cbd3d66ba50975fc65b8dff0
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2024-08-21 20:24:47 +0530

    show result conditionally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { faker } from '@faker-js/faker';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   paragraph: string = faker.lorem.paragraph();
+  enteredText: string = '';
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+  }
 }
